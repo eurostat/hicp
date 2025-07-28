48fba0054a792fb593d28ceebcb161fb2ddae638
sweinand <s.weinand90@googlemail.com>
2025-07-28 13:13:04 +0200

    introduced new settings

--- R/linking.r
+++ R/linking.r
@@ -2,11 +2,16 @@
 
 # Title:  Linking-in new index series
 # Author: Sebastian Weinand
-# Date:   5 February 2024
+# Date:   24 February 2025
 
 # link-in new index series:
-link <- function(x, x.new, t, t.overlap=NULL){
-
+link <- function(x, x.new, t, t.overlap=NULL, settings=list()){
+  
+  # set default settings if missing:
+  if(is.null(settings$chatty)) settings$chatty <- getOption("hicp.chatty")
+  if(is.null(settings$na.rm)) settings$na.rm <- FALSE
+  if(is.null(settings$freq)) settings$freq <- "auto"
+  
   # input checks:
   check.num(x=x)
   check.num(x=x.new)
@@ -14,69 +19,120 @@ link <- function(x, x.new, t, t.overlap=NULL){
   check.lengths(x=x, y=t)
   check.lengths(x=x, y=x.new)
   check.char(x=t.overlap, null.ok=TRUE, na.ok=FALSE)
-
+  check.log(x=settings$chatty, min.len=1, max.len=1, na.ok=FALSE)
+  check.log(x=settings$na.rm, min.len=1, max.len=1, na.ok=FALSE)
+  check.char(x=settings$freq, min.len=1, max.len=1, na.ok=FALSE)
+  
+  # match input:
+  freq <- match.arg(arg=settings$freq, choices=c("auto","month","quarter","year"))
+  
+  # frequency/number of periods per year:
+  if(freq=="auto"){
+    k <- nperiods(t)
+  }else{
+    if(freq=="month") k <- 12L
+    if(freq=="quarter") k <- 4L
+    if(freq=="year") k <- 1L
+  }
+  
+  # check chronological ordering, duplicates and gaps:
+  check.dateseries(x=t, freq=k, chatty=settings$chatty)
+  
+  # manipulate dates:
+  t <- as.Date(t, format="%Y-%m-%d") # set date format
+  y <- data.table::year(t) # get years
+  m <- data.table::month(t) # get months
+  ym <- yearmonth(y=y, m=m) # paste to year-month
+  # no use of pin.month() necessary; a user would set t.overlap 
+  # based on the dates/months present in t, irrespective if t is
+  # monthly, quarterly, or annual. if we would use pin.month(), 
+  # we would have to adjust t.overlap similarly to ensure matching
+  # between t and t.overlap
+  
   # check:
-  if(is.null(t.overlap)) check <- TRUE else check <- FALSE
+  if(is.null(t.overlap)){
+    check <- TRUE
+  }else{
+    check <- FALSE
+  }
 
-  # last value of current index:
+  # last value of current index defining the period
+  # when linking is carried out:
   t.last <- max(t[!is.na(x)])
-
-  # time period intersections:
-  t.avail <- as.Date(intersect(as.character(t[!is.na(x)]), as.character(t[!is.na(x.new)])))
-
-  # year period intersections:
-  y.avail <- tapply(X=t.avail, INDEX=format(t.avail, "%Y"), FUN=length)
-  y.avail <- names(y.avail[y.avail==12L])
-  y.avail <- t.avail[format(t.avail, "%Y")%in%y.avail]
-
+  
+  # compute annual averages for current and new index:
+  xavg <- navg(x=x, g=y, n=k, na.rm=settings$na.rm)
+  xavg.new <- navg(x=x.new, g=y, n=k, na.rm=settings$na.rm)
+  # keep in mind that the the averages xavg and xavg.new can be
+  # based on different sets if settings$na.rm=TRUE, e.g., if there
+  # are some NAs in x but not in x.new
+  
+  # compute linking factors:
+  lf <- c(
+    xavg/xavg.new, # annual overlap
+    stats::setNames(x/x.new, ym) # one-month or one-quarter overlap
+  )
+  
+  # drop NAs:
+  lf <- lf[!is.na(lf)]
+  
   # set overlap period if not provided:
-  if(is.null(t.overlap) & length(t.avail)>0){
-    t.overlap <- unique(c(format(y.avail, "%Y"), format(t.avail, "%Y-%m")))
+  if(is.null(t.overlap) & length(lf)>0){
+    t.overlap <- names(lf)
   }
-
-  # set each overlap period:
-  t.use <- vector(mode="list", length(t.overlap))
-  for(j in seq_along(t.overlap)){
-    t.use[[j]] <- c(y.avail[format(y.avail, "%Y")%in%t.overlap[j]], t.avail[format(t.avail, "%Y-%m")%in%t.overlap[j]])
-  }
-
-  # missing overlap periods:
-  keep <- lengths(t.use)>0
-  if(all(!keep)) message("No overlap period(s) found.")
-  if(!all(!keep) & any(!keep)) message("Some overlap period(s) not found.")
-
+  
+  # check if overlap periods are available:
+  keep <- t.overlap%in%names(lf)
+  
+  # print messages:
+  if(settings$chatty){
+    if(all(!keep)){
+      message("No overlap period(s) found.")
+    }
+    if(!all(!keep) && any(!keep)){
+      message("Some overlap period(s) in 't.overlap' not found.")
+    }
+  } 
+  
+  # match linking factors to overlap periods:
+  lf <- lf[match(x=t.overlap, table=names(lf))]
+  
   # output container:
   out <- matrix(data=x, byrow=FALSE, 
                 ncol=max(1, length(t.overlap)), nrow=length(t), 
                 dimnames=list(NULL, t.overlap))
-
-  # loop:
+  
+  # link old and new index series using all overlap periods:
   for(j in seq_along(t.overlap)[keep]){
-
-    # linked indices:
-    x.new.linked <- x.new[t>max(t.use[[j]], na.rm=TRUE)]*mean(x[t%in%t.use[[j]]], na.rm=TRUE)/mean(x.new[t%in%t.use[[j]]], na.rm=TRUE)
-
-    # old and linked index series:
-    out[,j] <- c(x[t<=t.last], x.new.linked[t[t>max(t.use[[j]], na.rm=TRUE)]>t.last])
-
+    out[,j] <- c(x[t<=t.last], (x.new*lf[j])[t>t.last])
   }
-
+  
+  # match to initial ordering if dates not chronological:
+  out[, keep] <- out[match(x=ym, table=c(ym[t<=t.last], ym[t>t.last])), keep, drop=FALSE]
+  
   # coerce to vector or keep matrix:
   if(ncol(out)<=1L){
-    if(!all(!keep) & check) message("Overlap period ", t.overlap, " used.")
+    if(settings$chatty && !all(!keep) && check){
+      message("Overlap period ", t.overlap, " used.")
+    }
     res <- as.vector(out[,1])
   }else{
     res <- out
   }
-
+  
   # return output:
   return(res)
-
+  
 }
 
 # compute level-shift factor:
-lsf <- function(x, x.new, t, t.overlap=NULL){
-
+lsf <- function(x, x.new, t, t.overlap=NULL, settings=list()){
+  
+  # set default settings if missing:
+  if(is.null(settings$chatty)) settings$chatty <- getOption("hicp.chatty")
+  if(is.null(settings$na.rm)) settings$na.rm <- FALSE
+  if(is.null(settings$freq)) settings$freq <- "auto"
+  
   # input checks:
   check.num(x=x)
   check.num(x=x.new)
@@ -84,45 +140,75 @@ lsf <- function(x, x.new, t, t.overlap=NULL){
   check.lengths(x=x, y=t)
   check.lengths(x=x, y=x.new)
   check.char(x=t.overlap, null.ok=TRUE, na.ok=FALSE)
-
-  # time period intersections:
-  t.avail <- as.Date(intersect(as.character(t[!is.na(x)]), as.character(t[!is.na(x.new)])))
-
-  # year period intersections:
-  y.avail <- tapply(X=t.avail, INDEX=format(t.avail, "%Y"), FUN=length)
-  y.avail <- names(y.avail[y.avail==12L])
-  y.avail <- t.avail[format(t.avail, "%Y")%in%y.avail]
-
-  # last december index values available in both index series:
-  t12 <- t.avail[format(t.avail, "%m")=="12"]
-  if(length(t12)>0) t12 <- max(t12, na.rm=TRUE)
-
-  # set overlap period if not provided:
-  if(is.null(t.overlap) & length(t.avail)>0){
-    t.overlap <- unique(c(format(y.avail, "%Y"), format(t.avail, "%Y-%m")))
+  check.log(x=settings$chatty, min.len=1, max.len=1, na.ok=FALSE)
+  check.log(x=settings$na.rm, min.len=1, max.len=1, na.ok=FALSE)
+  check.char(x=settings$freq, min.len=1, max.len=1, na.ok=FALSE)
+  
+  # match input:
+  freq <- match.arg(arg=settings$freq, choices=c("auto","month","quarter","year"))
+  
+  # frequency/number of periods per year:
+  if(freq=="auto"){
+    k <- nperiods(t)
+  }else{
+    if(freq=="month") k <- 12L
+    if(freq=="quarter") k <- 4L
+    if(freq=="year") k <- 1L
   }
+  
+  # check chronological ordering, duplicates and gaps:
+  check.dateseries(x=t, freq=k, chatty=settings$chatty)
+  
+  # manipulate dates:
+  t <- as.Date(t, format="%Y-%m-%d") # set date format
+  y <- data.table::year(t) # get years
+  m <- data.table::month(t) # get months
+  ym <- yearmonth(y=y, m=m) # paste to year-month
 
-  # set each overlap period:
-  t.use <- vector(mode="list", length(t.overlap))
-  for(j in seq_along(t.overlap)){
-    t.use[[j]] <- c(y.avail[format(y.avail, "%Y")%in%t.overlap[j]], t.avail[format(t.avail, "%Y-%m")%in%t.overlap[j]])
-  }
-
-  # missing overlap periods:
-  keep <- lengths(t.use)>0
-
-  # output container:
-  out <- rep(x=NA_real_, times=max(1, length(t.overlap)))
-  names(out) <- t.overlap
-
+  # index value available in both index series that would be
+  # used by the one-month or one-quarter overlap method. if the
+  # frequency is not monthly, pin.month() ensures that the month
+  # in the last quarter is set to december and, thus, matched:
+  m12 <- pin.month(t, freq=k)
+  t12 <- !is.na(x) & !is.na(x.new) & m12==12L
+  if(any(t12, na.rm=TRUE)) t12 <- max(y[t12], na.rm=TRUE)
+  idx <- y==t12 & m12==12L
+  
+  # compute annual averages for current and new index:
+  xavg <- navg(x=x, g=y, n=k, na.rm=settings$na.rm)
+  xavg.new <- navg(x=x.new, g=y, n=k, na.rm=settings$na.rm)
+  # keep in mind that the the averages xavg and xavg.new can be
+  # based on different sets if settings$na.rm=TRUE, e.g., if there
+  # are some NAs in x but not in x.new
+  
   # compute level-shift factors:
-  for(j in seq_along(t.overlap)[keep]){
-    out[j] <- (x.new[t%in%t12] / mean(x.new[t%in%t.use[[j]]], na.rm=TRUE)) / (x[t%in%t12] / mean(x[t%in%t.use[[j]]], na.rm=TRUE))
+  lf <- c(
+    (x.new[idx]/xavg.new) / (x[idx]/xavg), # annual overlap
+    (x.new[idx]/x.new) / (x[idx]/x) # one-month or one-quarter overlap
+  )
+  
+  # set names:
+  if(length(lf)>0) names(lf) <- c(names(xavg), ym)
+  
+  # drop NAs:
+  lf <- lf[!is.na(lf)]
+  
+  # set overlap period if not provided:
+  if(is.null(t.overlap) & length(lf)>0){
+    t.overlap <- names(lf)
   }
-
+  
+  # subset to requested overlap periods:
+  if(length(t.overlap)>0){
+    out <- lf[match(x=t.overlap, table=names(lf))]
+  }else{
+    out <- NA_real_
+  }
+  names(out) <- t.overlap
+  
   # return output to console:
   return(out)
-
+  
 }
 
 # END
--- tests/testthat/test_linking.r
+++ tests/testthat/test_linking.r
@@ -1,68 +1,230 @@
 # START
 
+options("hicp.chatty"=FALSE)
+
 # input data:
 t <- seq.Date(from=as.Date("2020-01-01"), to=as.Date("2023-12-01"), by="1 month")
 p <- rnorm(n=length(t), mean=100, sd=5) # current index
 p.new <- p+rnorm(n=length(p), mean=0, sd=1) # new index
 p[25:48] <- NA # current index stops in 2021
 
-res1 <- c(p[1:24], (p.new*p[t=="2021-12-01"]/p.new[t=="2021-12-01"])[25:48])
-res2 <- c(p[1:24], (p.new*mean(p[format(t,"%Y")=="2021"])/mean(p.new[format(t,"%Y")=="2021"]))[25:48])
-res3 <- c(p[1:24], (p.new*mean(p[format(t,"%Y")=="2022"])/mean(p.new[format(t,"%Y")=="2022"]))[25:48])
+# expected results for linked index series:
+res1 <- c(p[1:24], (p.new*mean(p[format(t,"%Y")=="2022"])/mean(p.new[format(t,"%Y")=="2022"]))[25:48]) # not available
+res2 <- c(p[1:24], (p.new*p[t=="2021-12-01"]/p.new[t=="2021-12-01"])[25:48])
+res3 <- c(p[1:24], (p.new*mean(p[format(t,"%Y")=="2021"])/mean(p.new[format(t,"%Y")=="2021"]))[25:48])
+res4 <- c(p[1:24], (p.new*p[t=="2021-06-01"]/p.new[t=="2021-06-01"])[25:48])
 
-expect_equal(
-  24+2,
-  ncol(link(x=p, x.new=p.new, t=t, t.overlap=NULL))
+# expected results for level-shift factor:
+lsf1 <- (mean(p[format(t,"%Y")=="2022"])/mean(p.new[format(t,"%Y")=="2022"])) # not available
+lsf2 <- p[t=="2021-12-01"]/p.new[t=="2021-12-01"] 
+lsf3 <- (mean(p[format(t,"%Y")=="2021"])/mean(p.new[format(t,"%Y")=="2021"]))
+lsf4 <- p[t=="2021-06-01"]/p.new[t=="2021-06-01"]
+
+
+### Tests on output format
+
+
+expect_true(
+  is.vector(link(x=p, x.new=p.new, t=t, t.overlap="2021"))
+)
+
+expect_true(
+  is.matrix(link(x=p, x.new=p.new, t=t, t.overlap=c("2021","2021-12")))
 )
 
 expect_equal(
-  as.matrix(data.table("2021-12"=res1, "2021"=res2, "2022"=res3)),
-  suppressMessages(link(x=p, x.new=p.new, t=t, t.overlap=c("2021-12","2021","2022")))
+  c(length(t), 24+2),
+  dim(link(x=p, x.new=p.new, t=t, t.overlap=NULL))
 )
 
-expect_equal(
-  res3,
-  suppressMessages(link(x=p, x.new=p.new, t=t, t.overlap="2022"))
+expect_true(
+  is.vector(lsf(x=p, x.new=p.new, t=t, t.overlap=NULL))
 )
-
-lsf1 <- p[t=="2021-12-01"]/p.new[t=="2021-12-01"]
-lsf2 <- (mean(p[format(t,"%Y")=="2021"])/mean(p.new[format(t,"%Y")=="2021"]))
-lsf3 <- (mean(p[format(t,"%Y")=="2022"])/mean(p.new[format(t,"%Y")=="2022"]))
 
 expect_equal(
   24+2,
   length(lsf(x=p, x.new=p.new, t=t, t.overlap=NULL))
 )
 
+
+### Consecutive time periods without gaps/breaks
+
+
+dt0 <- data.table("time"=t, "old"=p, "new"=p.new)
+
 expect_equal(
-  c("2021"=lsf2/lsf1, "9999"=NA, "2022"=lsf3/lsf1),
-  lsf(x=p, x.new=p.new, t=t, t.overlap=c("2021","9999","2022"))
+  as.matrix(data.table("2022"=res1, "2021-12"=res2, "2021"=res3, "2021-06"=res4)),
+  dt0[, link(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
 )
 
+expect_equal(
+  c("2022"=lsf1/lsf2, "2021-12"=1, "2021"=lsf3/lsf2, "2021-06"=lsf4/lsf2),
+  dt0[, lsf(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
 
 # no overlap periods available:
-p.new[1:24] <- NA # new index starts in 2022
+dt1 <- copy(dt0)
+dt1[1:24, "new":=NA] # new index starts in 2022
 
 expect_equal(
-  p,
-  suppressMessages(link(x=p, x.new=p.new, t=t, t.overlap=NULL))
+  dt1$old,
+  dt1[, link(x=old, x.new=new, t=time, t.overlap=NULL)]
 )
 
 expect_equal(
-  p,
-  suppressMessages(link(x=p, x.new=p.new, t=t, t.overlap="2022"))
+  dt1$old,
+  dt1[, link(x=old, x.new=new, t=time, t.overlap="2022")]
 )
 
 expect_equal(
   NA_real_,
-  lsf(x=p, x.new=p.new, t=t, t.overlap=NULL)
+  dt1[, lsf(x=old, x.new=new, t=time, t.overlap=NULL)]
 )
 
 expect_equal(
   c("2022"=NA_real_, "2022-12"=NA_real_),
-  lsf(x=p, x.new=p.new, t=t, t.overlap=c("2022","2022-12"))
+  dt1[, lsf(x=old, x.new=new, t=time, t.overlap=c("2022","2022-12"))]
 )
 
 
+### Time periods in random, non-chronological order without gaps/breaks
+
+
+# random ordering of time periods:
+idx <- sample(1:length(t))
+dt2 <- copy(dt0)[idx, ]
+
+expect_equal(
+  as.matrix(data.table("2022"=res1, "2021-12"=res2, "2021"=res3, "2021-06"=res4)),
+  dt2[, link(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))][order(idx),]
+)
+
+expect_equal(
+  c("2022"=lsf2/lsf1, "2021-12"=1, "2021"=lsf3/lsf2, "2021-06"=lsf4/lsf2),
+  dt2[, lsf(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
+
+
+### Dealing with breaks/gaps in time series
+
+
+dt3 <- copy(dt0)
+
+## (1) time periods available but index value missing (NA)
+idx <- dt3[, time%in%c("2021-01-01","2021-02-01")]
+dt3[idx, old:=NA]
+
+expect_equal(
+  as.matrix(data.table(
+    "2022"=dt3$old, 
+    "2021-12"=ifelse(dt3[, is.na(old) & time<"2022-01-01"], NA, res2), 
+    "2021"=dt3$old,
+    "2021-06"=ifelse(dt3[, is.na(old) & time<"2022-01-01"], NA, res4))),
+  a1 <- dt3[, link(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
+
+expect_equal(
+  c("2022"=NA, "2021-12"=1, "2021"=NA, "2021-06"=lsf4/lsf2),
+  a2 <- dt3[, lsf(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
+
+expect_equal(
+  dt3[, ifelse(time<"2022-01-01", old, new*mean(old[year(time)==2021], na.rm=TRUE)/mean(new[year(time)==2021], na.rm=TRUE))],
+  a3 <- dt3[, link(x=old, x.new=new, t=time, t.overlap="2021", settings=list(na.rm=TRUE))]
+)
+
+expect_equal(
+  c("2021"=dt3[, (mean(old[year(time)==2021], na.rm=TRUE)/mean(new[year(time)==2021], na.rm=TRUE)) / lsf2]),
+  a4 <- dt3[, lsf(x=old, x.new=new, t=time, t.overlap="2021", settings=list(na.rm=TRUE))]
+)
+
+## (2) time periods and index values not available
+dt4 <- dt3[!idx,]
+
+expect_equal(
+  as.matrix(data.table(
+    "2022"=dt4$old, 
+    "2021-12"=res2[!dt3[, is.na(old) & time<"2022-01-01"]], 
+    "2021"=dt4$old,
+    "2021-06"=res4[!dt3[, is.na(old) & time<"2022-01-01"]])),
+  b1 <- dt4[, link(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
+
+expect_equal(a1[!idx,], b1)
+
+expect_equal(
+  c("2022"=NA, "2021-12"=1, "2021"=NA, "2021-06"=lsf4/lsf2),
+  b2 <- dt4[, lsf(x=old, x.new=new, t=time, t.overlap=c("2022","2021-12","2021","2021-06"))]
+)
+
+expect_equal(a2, b2)
+
+expect_equal(
+  dt4[, ifelse(time<"2022-01-01", old, new*mean(old[year(time)==2021], na.rm=TRUE)/mean(new[year(time)==2021], na.rm=TRUE))],
+  b3 <- dt4[, link(x=old, x.new=new, t=time, t.overlap="2021", settings=list(na.rm=TRUE))]
+)
+
+expect_equal(
+  c("2021"=dt4[, (mean(old[year(time)==2021], na.rm=TRUE)/mean(new[year(time)==2021], na.rm=TRUE)) / lsf2]),
+  b4 <- dt4[, lsf(x=old, x.new=new, t=time, t.overlap="2021", settings=list(na.rm=TRUE))]
+)
+
+# if we drop the periods in idx even though there is an index
+# value present for p.new (which we drop as well), we cannot
+# expect that a3=b3 and a4=b4
+
+
+### Quarterly and annual data
+
+
+# quarterly data (note that days and months do not refer to last day in quarter):
+t <- seq.Date(from=as.Date("2020-02-01"), to=as.Date("2023-11-01"), by="3 month")
+t[length(t)] <- as.Date("2023-12-17")
+p <- rnorm(n=length(t), mean=100, sd=5) # current index
+p.new <- p+rnorm(n=length(p), mean=0, sd=1) # new index
+p[9:16] <- NA # current index stops in 2021
+
+expect_equal(
+  cbind(
+    "2020-12"=c(p[1:8], rep(NA, 8)),
+    "2020"=c(p[1:8], (p.new*mean(p[1:4])/mean(p.new[1:4]))[9:16]),
+    "2020-11"=c(p[1:8], (p.new*mean(p[t=="2020-11-01"])/mean(p.new[t=="2020-11-01"]))[9:16])
+    ),
+  link(x=p, x.new=p.new, t=t, t.overlap=c("2020-12","2020","2020-11"))
+)
+
+expect_equal(
+  c(
+    "2020-12"=NA,
+    "2020"=(mean(p[1:4])/mean(p.new[1:4]))/(p[t=="2021-11-01"]/p.new[t=="2021-11-01"]),
+    "2020-11"=(p[t=="2020-11-01"]/p.new[t=="2020-11-01"])/(p[t=="2021-11-01"]/p.new[t=="2021-11-01"])
+  ),
+  lsf(x=p, x.new=p.new, t=t, t.overlap=c("2020-12","2020","2020-11"))
+)
+
+# annual data (note that days and months do not refer to last day in quarter):
+t <- seq.Date(from=as.Date("2020-02-01"), to=as.Date("2025-02-01"), by="12 month")
+t[length(t)] <- as.Date("2025-08-13")
+p <- rnorm(n=length(t), mean=100, sd=5) # current index
+p.new <- p+rnorm(n=length(p), mean=0, sd=1) # new index
+p[4:6] <- NA # current index stops in 2022
+
+expect_equal(
+  cbind(
+    "2020-12"=c(p[1:3], rep(NA, 3)),
+    "2020"=c(p[1:3], (p.new*p[t=="2020-02-01"]/p.new[t=="2020-02-01"])[4:6]),
+    "2021-02"=c(p[1:3], (p.new*p[t=="2021-02-01"]/p.new[t=="2021-02-01"])[4:6])
+  ),
+  link(x=p, x.new=p.new, t=t, t.overlap=c("2020-12","2020","2021-02"))
+)
+
+expect_equal(
+  c(
+    "2020-12"=NA,
+    "2020"=(p[t=="2020-02-01"]/p.new[t=="2020-02-01"])/(p[t=="2022-02-01"]/p.new[t=="2022-02-01"]),
+    "2021-02"=(p[t=="2021-02-01"]/p.new[t=="2021-02-01"])/(p[t=="2022-02-01"]/p.new[t=="2022-02-01"])
+  ),
+  lsf(x=p, x.new=p.new, t=t, t.overlap=c("2020-12","2020","2021-02"))
+)
 
 # END
