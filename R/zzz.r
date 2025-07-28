d44f561bfca03d7cb8448088d9abe34e2774dce2
sweinand <s.weinand90@googlemail.com>
2025-07-28 13:03:02 +0200

    updated package options at start

--- R/zzz.r
+++ R/zzz.r
@@ -2,18 +2,20 @@
 
 # Title:  Package options
 # Author: Sebastian Weinand
-# Date:   18 July 2024
+# Date:   22 July 2025
 
 .onLoad <- function(libname, pkgname){
-  options(hicp.unbundle=TRUE) # unbundle coicop bundle codes
-  options(hicp.coicop.version="ecoicop-hicp") # set coicop version used by functions
+  options(hicp.chatty=TRUE) # print package internal messages and warnings
+  options(hicp.coicop.version="ecoicop.hicp") # set coicop version used by functions
   options(hicp.all.items.code="00") # set code of all-items index
+  options(hicp.coicop.bundles=coicop.bundles) # coicop bundle dictionary
 }
 
 .onAttach <- function(libname, pkgname){
-  options(hicp.unbundle=TRUE) 
-  options(hicp.coicop.version="ecoicop-hicp")
+  options(hicp.chatty=TRUE)
+  options(hicp.coicop.version="ecoicop.hicp")
   options(hicp.all.items.code="00")
+  options(hicp.coicop.bundles=coicop.bundles)
 }
 
 # END
