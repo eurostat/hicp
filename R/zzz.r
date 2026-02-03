d691803f767a5fab1b25f8a48f53357df73669f9
sweinand <s.weinand90@googlemail.com>
2026-02-03 13:25:15 +0100

    updated default options

--- R/zzz.r
+++ R/zzz.r
@@ -2,20 +2,20 @@
 
 # Title:  Package options
 # Author: Sebastian Weinand
-# Date:   22 July 2025
+# Date:   28 January 2026
 
 .onLoad <- function(libname, pkgname){
   options(hicp.chatty=TRUE) # print package internal messages and warnings
-  options(hicp.coicop.version="ecoicop.hicp") # set coicop version used by functions
-  options(hicp.all.items.code="00") # set code of all-items index
-  options(hicp.coicop.bundles=coicop.bundles) # coicop bundle dictionary
+  options(hicp.coicop.version="ecoicop2.hicp") # set COICOP version used by functions
+  options(hicp.coicop.prefix="CP") # prefix of COICOP codes
+  options(hicp.all.items.code="TOTAL") # set code of all-items index
 }
 
 .onAttach <- function(libname, pkgname){
   options(hicp.chatty=TRUE)
-  options(hicp.coicop.version="ecoicop.hicp")
-  options(hicp.all.items.code="00")
-  options(hicp.coicop.bundles=coicop.bundles)
+  options(hicp.coicop.version="ecoicop2.hicp")
+  options(hicp.coicop.prefix="CP")
+  options(hicp.all.items.code="TOTAL")
 }
 
 # END
